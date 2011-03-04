712e369ede98fac0b0c062e92b9a4bbd344ed55e
William Dyson <william.dyson@cito.co>
2011-03-04 17:29:57 +0000

    Created Blank Game Class

---
+++ src/Game.h
@@ -0,0 +1,17 @@
+#include "TileSet.h"
+#include "Player.h"
+#include "Database.h"
+
+class Game
+{
+public:
+	void commit();
+	void end();
+
+	Player p1;
+	Player p2;
+	Player p3;
+	Player p4;
+private:
+
+};
