6360f0a8c0eb90e465c5290daf267b1213c5f797
William Dyson <william.dyson@cito.co>
2011-03-04 20:11:18 +0000

    Added Remaining USAGE Commands

--- src/main.cpp
+++ src/main.cpp
@@ -24,33 +24,102 @@ int main(int argc, char* argv[])
 	if(argc >= 5 && strcmp(argv[2], "add") == 0)
 	{
 		Database* db = new Database(argv[1]);
-
 		Game game(db);
 
 		// Player number check
 		if(strcmp(argv[3], "1") == 0)
 		{
 			game.p1->push(argc, argv);
-			cout << game.p1->get(game.p1->count() - 1)->toString();
 		}
 
 		if(strcmp(argv[3], "2") == 0)
 		{
 			game.p2->push(argc, argv);
-			cout << game.p2->get(game.p2->count() - 1)->toString();
 		}
 
 		if(strcmp(argv[3], "3") == 0)
 		{
 			game.p3->push(argc, argv);
-			cout << game.p3->get(game.p3->count() - 1)->toString();
 		}
 
 		if(strcmp(argv[3], "4") == 0)
 		{
 			game.p4->push(argc, argv);
-			cout << game.p4->get(game.p4->count() - 1)->toString();
 		}
+
+		game.save();
+
+		delete db;
+	}
+
+	if(argc >= 3 && strcmp(argv[2], "commit") == 0)
+	{
+		Database* db = new Database(argv[1]);
+		Game game(db);
+
+		cout << "Ending Round" << endl;
+
+		game.commit();
+		game.save();
+
+		delete db;
+	}
+
+	if(argc >= 3 && strcmp(argv[2], "end") == 0)
+	{
+		Database* db = new Database(argv[1]);
+		Game game(db);
+
+		cout << "Ending Game" << endl;
+
+		game.end();
+		game.save();
+
+		delete db;
+	}
+
+	if(argc >= 3 && strcmp(argv[2], "stat") == 0)
+	{
+		Database* db = new Database(argv[1]);
+		Game game(db);
+
+		cout << game.status();
+		game.save();
+
+		delete db;
+	}
+
+	if(argc >= 3 && strcmp(argv[2], "reset") == 0)
+	{
+		Database* db = new Database(argv[1]);
+		Game game(db);
+
+		// Player number check
+		if(strcmp(argv[3], "1") == 0)
+		{
+			cout << "Reset Player 1" << endl;
+			game.p1->reset();
+		}
+
+		if(strcmp(argv[3], "2") == 0)
+		{
+			cout << "Reset Player 2" << endl;
+			game.p2->reset();
+		}
+
+		if(strcmp(argv[3], "3") == 0)
+		{
+			cout << "Reset Player 3" << endl;
+			game.p3->reset();
+		}
+
+		if(strcmp(argv[3], "4") == 0)
+		{
+			cout << "Reset Player 4" << endl;
+			game.p4->reset();
+		}
+
+		game.save();
 
 		delete db;
 	}
